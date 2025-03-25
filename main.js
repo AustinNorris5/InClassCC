226e0b0639e8fc5d5a65c8e113b00bcafde121ae
AustinNorris5 <ajnorris@usf.edu>
2025-03-25 16:28:41 -0400

    fixed api

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-const BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
-
-export async function getPosts() {
-    try{
-        const rea = await fetch(BASE_URL);
-
-        if (!resizeBy.ok) {
-            throw new Error(`Error: ${resizeBy.status}`);
-        }
-        return await resizeBy.json();
-     } catch (eer) {
-            console.error(`API Fetch Failed:`, eer.message);
-            throw eer
-        }
-} 
 
 import {getPosts} from `./api.js`;
 
